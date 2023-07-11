618905551532356314d295def15bf9f9e14ae7ef
James Bellomy <jay.bells12@gmail.com>
2023-07-11 16:30:49 -0700

    Messing around with dynamic imports?

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,17 +1,27 @@
-"use client";
+'use client';
 
-import useSwr from "swr";
-import { Box, Heading } from "@chakra-ui/react";
-import { Logo } from "@components/Logo";
+import useSwr from 'swr';
+import { Box, Heading } from '@chakra-ui/react';
+import { Logo } from '@components/Logo';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
+export const createGoogleFontModule = async (fontName: string) => {
+  const module = await import('../../../node_modules/next/font/google');
+  const font = module.Inter;
+  console.log(module);
+  console.log(module.Inter);
+};
+
 export default function Page() {
-  const { data, error } = useSwr("/api/bookings", fetcher);
+  const { data, error } = useSwr('/api/bookings', fetcher);
 
   if (error) {
     return (
-      <Heading as={"h1"} onClick={() => console.log(error)}>
+      <Heading
+        as={'h1'}
+        onClick={() => console.log(error)}
+      >
         Error, Failed to load data
       </Heading>
     );
@@ -19,5 +29,11 @@ export default function Page() {
     return <h1>Loading...</h1>;
   }
 
-  return <Logo width={"500px"} height={"300px"} />;
+  return (
+    <Logo
+      width={'500px'}
+      height={'300px'}
+      onClick={() => createGoogleFontModule('Inter')}
+    />
+  );
 }
