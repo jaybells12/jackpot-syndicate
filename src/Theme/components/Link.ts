f26a3771861adc4780d884ffff66010153329da3
James Bellomy <jay.bells12@gmail.com>
2023-07-11 16:43:27 -0700

    Add primary variant to link component

--- src/Theme/components/Link.ts
+++ src/Theme/components/Link.ts
@@ -13,6 +13,25 @@ const variants = {
       textDecoration: "none",
     },
   }),
+  primary: defineStyle({
+    width: "fit-content",
+    fontSize: "1rem",
+    lineHeight: "1.5",
+    backgroundColor: "transparent",
+    color: "text.light",
+    borderColor: "button.dark.accent",
+    borderWidth: "1px",
+    borderRadius: "8px",
+    padding: "1em 1.5em",
+    textTransform: "uppercase",
+    _hover: {
+      backgroundColor: "button.dark.primary",
+      borderColor: "button.dark.primary",
+      textDecoration: "none",
+      color: "text.hover",
+      cursor: "pointer",
+    },
+  }),
   contact: defineStyle({
     fontSize: "1rem", // 0.75rem on Figma
     fontStyle: "normal",
