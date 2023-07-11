3510aa55a69e92e5e10d4950900466842aeffed6
James Bellomy <jay.bells12@gmail.com>
2023-07-11 16:34:16 -0700

    Add footer semantic element to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client'
 
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
-import interaction from "@fullcalendar/interaction";
-import { features } from "src/data/staticData";
-import { NavBar } from "@components/NavBar";
-import { Hero } from "@components/Hero";
-import { Link } from "@chakra-ui/next-js";
-import { FeaturesItem } from "@components/FeaturesItem";
-import { Button, useDisclosure } from "@chakra-ui/react";
-import { Features } from "@components/Features";
-import { Contact } from "@components/Contact";
+// import FullCalendar from '@fullcalendar/react'
+// import dayGridPlugin from '@fullcalendar/daygrid'
+// import interaction from '@fullcalendar/interaction'
+// import { features } from 'src/data/staticData'
+// import { Link } from '@chakra-ui/next-js'
+// import { FeaturesItem } from '@components/FeaturesItem'
+// import { Button, useDisclosure } from '@chakra-ui/react'
+import { NavBar } from '@components/NavBar'
+import { Hero } from '@components/Hero'
+import { Features } from '@components/Features'
+import { Contact } from '@components/Contact'
 
 export default function Home() {
   return (
@@ -22,8 +22,10 @@ export default function Home() {
         <Hero />
         <Features />
       </main>
-      <Contact />
+      <footer>
+        <Contact />
+      </footer>
     </>
-  );
+  )
 }
 
