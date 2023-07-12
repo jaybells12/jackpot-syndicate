dd2bc0839ec0eec7c102f719c1c6ed8b9d8f7be0
James Bellomy <jay.bells12@gmail.com>
2023-07-11 20:59:49 -0700

    Create TempImage Component until Next/Chakra fix

---
+++ src/components/TempImage/TempImage.tsx
@@ -0,0 +1,24 @@
+import { chakra, ChakraComponent } from '@chakra-ui/react'
+import NextImage, { ImageProps as NextImageProps } from 'next/image'
+// This fix is temporary until chakra/next-js play nice together again
+export const TempImage: ChakraComponent<'img', NextImageProps> = chakra(
+  NextImage,
+  {
+    shouldForwardProp: (prop) =>
+      [
+        'src',
+        'alt',
+        'width',
+        'height',
+        'fill',
+        'loader',
+        'quality',
+        'priority',
+        'loading',
+        'placeholder',
+        'blurDataURL',
+        'unoptimized',
+        'onLoadingComplete',
+      ].includes(prop),
+  }
+)
