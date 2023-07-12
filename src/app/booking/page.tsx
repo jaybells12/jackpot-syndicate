056b00eabde3a85b81650960dfa580eb36e20dd7
James Bellomy <jay.bells12@gmail.com>
2023-07-11 22:05:53 -0700

    Page must export a module

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,10 +1,10 @@
-// 'use client'
+'use client'
 
-// import useSwr from 'swr'
-// import { Box, Heading } from '@chakra-ui/react'
-// import { Logo } from '@components/Logo'
+import useSwr from 'swr'
+import { Box, Heading } from '@chakra-ui/react'
+import { Logo } from '@components/Logo'
 
-// const fetcher = (url: string) => fetch(url).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 // export const createGoogleFontModule = async (fontName: string) => {
 //   const module = await import('../../../node_modules/next/font/google')
@@ -13,27 +13,26 @@
 //   console.log(module.Inter)
 // }
 
-// export default function Page() {
-//   const { data, error } = useSwr('/api/bookings', fetcher)
+export default function Page() {
+  //   const { data, error } = useSwr('/api/bookings', fetcher)
 
-//   if (error) {
-//     return (
-//       <Heading
-//         as={'h1'}
-//         onClick={() => console.log(error)}
-//       >
-//         Error, Failed to load data
-//       </Heading>
-//     )
-//   } else if (!data) {
-//     return <h1>Loading...</h1>
-//   }
+  //   if (error) {
+  //     return (
+  //       <Heading
+  //         as={'h1'}
+  //         onClick={() => console.log(error)}
+  //       >
+  //         Error, Failed to load data
+  //       </Heading>
+  //     )
+  //   } else if (!data) {
+  //     return <h1>Loading...</h1>
+  //   }
 
-//   return (
-//     <Logo
-//       width={'500px'}
-//       height={'300px'}
-//       onClick={() => createGoogleFontModule('Inter')}
-//     />
-//   )
-// }
+  return (
+    <Logo
+      width={'500px'}
+      height={'300px'}
+    />
+  )
+}
