94c22189030ec1a01d65713bb67daea94e4eb383
James Bellomy <jay.bells12@gmail.com>
2023-07-11 21:01:55 -0700

    Add url links to staticData

--- src/data/staticData.ts
+++ src/data/staticData.ts
@@ -9,27 +9,31 @@ const features = [
   {
     title: 'Live Stage',
     image: liveStageImg,
+    url: '/sound-stage',
   },
   {
     title: 'Podcast Studio',
     image: podcastStudioImg,
+    url: '/podcase-room',
   },
   {
     title: 'Youtube Studio',
     image: youtubeStudioImg,
+    url: '/youtube-studio',
   },
   {
     title: 'Recording Studio / ISO Booth',
     image: recordingStudioImg,
+    url: '/recording-studio',
   },
-  {
-    title: 'Live Broadcasts & Events',
-    image: liveBroadcastImg,
-  },
-  {
-    title: 'Gear Rental List',
-    image: gearRentalImg,
-  },
+  // {
+  //   title: 'Live Broadcasts & Events',
+  //   image: liveBroadcastImg,
+  // },
+  // {
+  //   title: 'Gear Rental List',
+  //   image: gearRentalImg,
+  // },
 ]
 
 const navLinks = [
