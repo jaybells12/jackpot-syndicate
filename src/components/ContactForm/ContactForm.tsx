18c673d07a5e591e9143beeb4eb5a48a8626d512
James Bellomy <jay.bells12@gmail.com>
2023-07-12 22:18:08 -0700

    Add form field verification & validation

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -7,8 +7,8 @@ import {
   Button,
   FlexProps,
 } from '@chakra-ui/react'
-
-import { ChangeEvent, useReducer, useState, ReactElement } from 'react'
+import sendEmail from 'src/utils/sendEmail'
+import { ChangeEvent, MouseEvent, useReducer, useState } from 'react'
 
 function reducer(state: FormErrorState, action: FormErrorAction) {
   switch (action.type) {
@@ -71,25 +71,90 @@ export const ContactForm = (props: FlexProps) => {
     switch (field) {
       case 'first':
         setFirst(value)
+        if (value && isError.first) {
+          dispatch({ type: 'first', payload: false })
+        } else if (!value && !isError.first) {
+          dispatch({ type: 'first', payload: true })
+        }
         break
       case 'last':
         setLast(value)
+        if (value && isError.last) {
+          dispatch({ type: 'last', payload: false })
+        } else if (!value && !isError.last) {
+          dispatch({ type: 'last', payload: true })
+        }
         break
       case 'email':
         setEmail(value)
+        if (value && isError.email) {
+          dispatch({ type: 'email', payload: false })
+        } else if (!value && !isError.email) {
+          dispatch({ type: 'email', payload: true })
+        }
         break
       case 'phone':
         setPhone(value)
         break
       case 'subject':
         setSubject(value)
+        if (value && isError.subject) {
+          dispatch({ type: 'subject', payload: false })
+        } else if (!value && !isError.subject) {
+          dispatch({ type: 'subject', payload: true })
+        }
         break
       case 'message':
         setMessage(value)
+        if (value && isError.message) {
+          dispatch({ type: 'message', payload: false })
+        } else if (!value && !isError.message) {
+          dispatch({ type: 'message', payload: true })
+        }
         break
       default:
         console.log('This should never happen')
     }
+  }
+
+  // If an error property is true, then return false
+  const isValid = () => {
+    return !(
+      isError.first ||
+      isError.last ||
+      isError.email ||
+      isError.subject ||
+      isError.message
+    )
+  }
+
+  const verifyRequiredFields = () => {
+    first
+      ? dispatch({ type: 'first', payload: false })
+      : dispatch({ type: 'first', payload: true })
+    last
+      ? dispatch({ type: 'last', payload: false })
+      : dispatch({ type: 'last', payload: true })
+    email
+      ? dispatch({ type: 'email', payload: false })
+      : dispatch({ type: 'email', payload: true })
+    subject
+      ? dispatch({ type: 'subject', payload: false })
+      : dispatch({ type: 'subject', payload: true })
+    message
+      ? dispatch({ type: 'message', payload: false })
+      : dispatch({ type: 'message', payload: true })
+  }
+
+  const handleSubmit = (e: MouseEvent) => {
+    e.preventDefault()
+    verifyRequiredFields()
+    const validated = isValid()
+    if (validated) {
+      sendEmail({ first, last, email, phone, subject, message })
+      //At this point, need to do something to hide the form and render a thank you message
+    }
+    //if this point is reached, attempt is invalid, render a modal explaining issue?
   }
 
   return (
@@ -117,7 +182,9 @@ export const ContactForm = (props: FlexProps) => {
             value={first}
             onChange={handleInput}
           />
-          <FormErrorMessage>First name is required.</FormErrorMessage>
+          <FormErrorMessage position={'absolute'}>
+            First name is required.
+          </FormErrorMessage>
         </FormControl>
         <FormControl
           as={'fieldset'}
@@ -135,7 +202,9 @@ export const ContactForm = (props: FlexProps) => {
             value={last}
             onChange={handleInput}
           />
-          <FormErrorMessage>Last name is required.</FormErrorMessage>
+          <FormErrorMessage position={'absolute'}>
+            Last name is required.
+          </FormErrorMessage>
         </FormControl>
       </Flex>
       <Flex
@@ -158,7 +227,9 @@ export const ContactForm = (props: FlexProps) => {
             value={email}
             onChange={handleInput}
           />
-          <FormErrorMessage>Email is required.</FormErrorMessage>
+          <FormErrorMessage position={'absolute'}>
+            Email is required.
+          </FormErrorMessage>
         </FormControl>
         <FormControl
           as={'fieldset'}
@@ -176,7 +247,9 @@ export const ContactForm = (props: FlexProps) => {
             value={phone}
             onChange={handleInput}
           />
-          <FormErrorMessage>Phone number is required.</FormErrorMessage>
+          <FormErrorMessage position={'absolute'}>
+            Phone number is required.
+          </FormErrorMessage>
         </FormControl>
       </Flex>
       <FormControl
@@ -196,7 +269,9 @@ export const ContactForm = (props: FlexProps) => {
           value={subject}
           onChange={handleInput}
         />
-        <FormErrorMessage>Subject is required.</FormErrorMessage>
+        <FormErrorMessage position={'absolute'}>
+          Subject is required.
+        </FormErrorMessage>
       </FormControl>
       <FormControl
         as={'fieldset'}
@@ -215,12 +290,15 @@ export const ContactForm = (props: FlexProps) => {
           value={message}
           onChange={handleInput}
         />
-        <FormErrorMessage>Message is required.</FormErrorMessage>
+        <FormErrorMessage position={'absolute'}>
+          Message is required.
+        </FormErrorMessage>
       </FormControl>
       <Button
         variant={'contact'}
         alignSelf={'flex-end'}
         marginTop={'2rem'}
+        onClick={handleSubmit}
       >
         Send Message
       </Button>
