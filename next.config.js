4c492d154897106dc6bce84e2f164cb18a1a71d0
James Bellomy <jay.bells12@gmail.com>
2023-08-09 23:00:17 -0700

    Add CSP header

--- next.config.js
+++ next.config.js
@@ -1,9 +1,41 @@
 /** @type {import('next').NextConfig} */
+// const ContentSecurityPolicy = `
+// default-src 'self';
+// frame-src 'self' player.vimeo.com 19vod-adaptive.akamaized.net i.vimeocdn.com;
+// `
+
+const ContentSecurityPolicy = `
+ default-src 'self';
+ font-src 'self';
+ img-src 'self';
+ script-src 'self' 'unsafe-inline' 'unsafe-eval' player.vimeo.com;
+ style-src 'self' 'unsafe-inline';
+ frame-src 'self' player.vimeo.com 19vod-adaptive.akamaized.net;
+`
+
+console.log(JSON.stringify(ContentSecurityPolicy))
+console.log(
+  JSON.stringify(ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim())
+)
+
 const nextConfig = {
+  trailingSlash: true,
   experimental: {
     serverActions: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ]
   },
 }
 
 module.exports = nextConfig
-
