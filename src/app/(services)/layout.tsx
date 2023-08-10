7c059153ae36bcea6caaad435648454b656a5d22
James Bellomy <jay.bells12@gmail.com>
2023-08-09 21:01:18 -0700

    Fix dynamic import at service layout layer

--- src/app/(services)/layout.tsx
+++ src/app/(services)/layout.tsx
@@ -1,12 +1,16 @@
 'use client'
 
-import { OffsetTopHandle } from '@components/Contact'
+import { ContactProps, OffsetTopHandle } from '@components/Contact'
 import { NavBar } from '@components/NavBar'
 import dynamic from 'next/dynamic'
 import { useRef } from 'react'
 
 const Contact = dynamic(() =>
   import('@components/Contact').then((module) => module.Contact)
+)
+
+const WrappedContact = ({ forwardedRef }: ContactProps) => (
+  <Contact forwardedRef={forwardedRef} />
 )
 
 export default function ServicesLayout({
@@ -20,7 +24,7 @@ export default function ServicesLayout({
     <>
       <NavBar footerRef={footerRef} />
       <main>{children}</main>
-      <Contact ref={footerRef} />
+      <WrappedContact forwardedRef={footerRef} />
     </>
   )
 }
