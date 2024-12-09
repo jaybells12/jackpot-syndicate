d501191a264c61c2648783817d53603329237233
jbells <jay.bells12@gmail.com>
2024-12-09 05:32:12 -0800

    Remove logging

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,62 +1,45 @@
-import {
-  Flex,
-  Input,
-  FormControl,
-  Button,
-  Text,
-  FlexProps,
-} from "@chakra-ui/react";
-import { useSize } from "@chakra-ui/react-use-size";
-import {
-  ChangeEvent,
-  MouseEvent,
-  useCallback,
-  useReducer,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import { Flex, Input, FormControl, Button, Text, FlexProps } from '@chakra-ui/react';
+import { useSize } from '@chakra-ui/react-use-size';
+import { ChangeEvent, MouseEvent, useCallback, useReducer, useEffect, useRef, useState } from 'react';
 // import ReCAPTCHA from 'react-google-recaptcha'
-import { Link } from "@chakra-ui/next-js";
-import verifyRecaptcha from "src/utils/verifyRecaptcha";
-import sendEmail from "src/utils/sendEmail";
-import { ContactFormField } from "./ContactFormField";
-import { PaperPlane } from "@components/PaperPlane";
-import { ContactFormResults } from "./ContactFormResults";
-import dynamic from "next/dynamic";
+import { Link } from '@chakra-ui/next-js';
+import verifyRecaptcha from 'src/utils/verifyRecaptcha';
+import sendEmail from 'src/utils/sendEmail';
+import { ContactFormField } from './ContactFormField';
+import { PaperPlane } from '@components/PaperPlane';
+import { ContactFormResults } from './ContactFormResults';
+import dynamic from 'next/dynamic';
 
-const ReCAPTCHA = dynamic(() =>
-  import("@components/LazyReCaptcha").then((mod) => mod.LazyReCaptcha),
-);
+const ReCAPTCHA = dynamic(() => import('@components/LazyReCaptcha').then((mod) => mod.LazyReCaptcha));
 
 function reducer(state: FormErrorState, action: FormErrorAction) {
   switch (action.type) {
-    case "first":
+    case 'first':
       return {
         ...state,
         first: action.payload,
       };
-    case "last":
+    case 'last':
       return {
         ...state,
         last: action.payload,
       };
-    case "email":
+    case 'email':
       return {
         ...state,
         email: action.payload,
       };
-    case "phone":
+    case 'phone':
       return {
         ...state,
         phone: action.payload,
       };
-    case "subject":
+    case 'subject':
       return {
         ...state,
         subject: action.payload,
       };
-    case "message":
+    case 'message':
       return {
         ...state,
         message: action.payload,
@@ -67,7 +50,7 @@ function reducer(state: FormErrorState, action: FormErrorAction) {
     //     message: action.payload,
     //   }
     default:
-      throw Error("Unknown Form Action: ", action.type);
+      throw Error('Unknown Form Action: ', action.type);
   }
 }
 
@@ -83,12 +66,12 @@ export const ContactForm = (props: FlexProps) => {
   });
   // // Address is a honeypot field
   // const [address, setaddress] = useState('')
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
   const [result, setResult] = useState(false);
   const [showResult, setShowResult] = useState(false);
   const [isDisabled, setIsDisabled] = useState(false);
@@ -97,18 +80,11 @@ export const ContactForm = (props: FlexProps) => {
   const formRef = useRef<HTMLElement>(null);
   const formSize = useSize(formRef);
 
-  const FIELD_SPACING = "1rem";
+  const FIELD_SPACING = '1rem';
 
   // If an error property is true, then return false
   const isValid = useCallback(() => {
-    return !(
-      isError.first ||
-      isError.last ||
-      isError.email ||
-      isError.phone ||
-      isError.subject ||
-      isError.message
-    );
+    return !(isError.first || isError.last || isError.email || isError.phone || isError.subject || isError.message);
   }, [isError]);
 
   useEffect(() => {
@@ -151,85 +127,70 @@ export const ContactForm = (props: FlexProps) => {
     }
 
     switch (field) {
-      case "first":
+      case 'first':
         setFirst(value);
         if (value && isError.first) {
-          dispatch({ type: "first", payload: false });
+          dispatch({ type: 'first', payload: false });
         } else if (!value && !isError.first) {
-          dispatch({ type: "first", payload: true });
+          dispatch({ type: 'first', payload: true });
         }
         break;
-      case "last":
+      case 'last':
         setLast(value);
         if (value && isError.last) {
-          dispatch({ type: "last", payload: false });
+          dispatch({ type: 'last', payload: false });
         } else if (!value && !isError.last) {
-          dispatch({ type: "last", payload: true });
+          dispatch({ type: 'last', payload: true });
         }
         break;
-      case "email":
+      case 'email':
         setEmail(value);
         if (value && isError.email) {
-          dispatch({ type: "email", payload: false });
+          dispatch({ type: 'email', payload: false });
         } else if (!value && !isError.email) {
-          dispatch({ type: "email", payload: true });
+          dispatch({ type: 'email', payload: true });
         }
         break;
-      case "phone":
+      case 'phone':
         setPhone(value);
         if (value && isError.phone) {
-          dispatch({ type: "phone", payload: false });
+          dispatch({ type: 'phone', payload: false });
         } else if (!value && !isError.phone) {
-          dispatch({ type: "phone", payload: true });
+          dispatch({ type: 'phone', payload: true });
         }
         break;
-      case "subject":
+      case 'subject':
         setSubject(value);
         if (value && isError.subject) {
-          dispatch({ type: "subject", payload: false });
+          dispatch({ type: 'subject', payload: false });
         } else if (!value && !isError.subject) {
-          dispatch({ type: "subject", payload: true });
+          dispatch({ type: 'subject', payload: true });
         }
         break;
-      case "message":
+      case 'message':
         setMessage(value);
         if (value && isError.message) {
-          dispatch({ type: "message", payload: false });
+          dispatch({ type: 'message', payload: false });
         } else if (!value && !isError.message) {
-          dispatch({ type: "message", payload: true });
+          dispatch({ type: 'message', payload: true });
         }
         break;
       default:
-        console.log("This should never happen");
+        console.log('This should never happen');
     }
   };
 
   const verifyRequiredFields = () => {
-    first
-      ? dispatch({ type: "first", payload: false })
-      : dispatch({ type: "first", payload: true });
-    last
-      ? dispatch({ type: "last", payload: false })
-      : dispatch({ type: "last", payload: true });
-    email
-      ? dispatch({ type: "email", payload: false })
-      : dispatch({ type: "email", payload: true });
-    phone
-      ? dispatch({ type: "phone", payload: false })
-      : dispatch({ type: "phone", payload: true });
-    subject
-      ? dispatch({ type: "subject", payload: false })
-      : dispatch({ type: "subject", payload: true });
-    message
-      ? dispatch({ type: "message", payload: false })
-      : dispatch({ type: "message", payload: true });
+    first ? dispatch({ type: 'first', payload: false }) : dispatch({ type: 'first', payload: true });
+    last ? dispatch({ type: 'last', payload: false }) : dispatch({ type: 'last', payload: true });
+    email ? dispatch({ type: 'email', payload: false }) : dispatch({ type: 'email', payload: true });
+    phone ? dispatch({ type: 'phone', payload: false }) : dispatch({ type: 'phone', payload: true });
+    subject ? dispatch({ type: 'subject', payload: false }) : dispatch({ type: 'subject', payload: true });
+    message ? dispatch({ type: 'message', payload: false }) : dispatch({ type: 'message', payload: true });
   };
 
   const handleSubmit = (e: MouseEvent) => {
     e.preventDefault();
-    console.log("RecapNeeded: ", recapNeeded);
-    console.log("RecapRef: ", recaptchaValue.current);
-
     // Validate Form Fields
     verifyRequiredFields();
     // When component rerenders, submission logic continues when isDisabled is true
@@ -270,9 +231,9 @@ export const ContactForm = (props: FlexProps) => {
   return (
     <Flex
       ref={formRef}
-      position={"relative"}
-      direction={"column"}
-      margin={["2.25em 1em", null, "2.25em 1.75em 2.25em 1em"]}
+      position={'relative'}
+      direction={'column'}
+      margin={['2.25em 1em', null, '2.25em 1.75em 2.25em 1em']}
       {...props}
     >
       <ContactFormResults
@@ -283,14 +244,14 @@ export const ContactForm = (props: FlexProps) => {
         tryAgain={() => setShowResult(false)}
       />
       <Flex
-        gap={[FIELD_SPACING, null, "1.5rem"]}
-        direction={["column", null, "row"]}
+        gap={[FIELD_SPACING, null, '1.5rem']}
+        direction={['column', null, 'row']}
       >
         <ContactFormField
           isRequired
           isDisabled={isDisabled}
           isInvalid={isError.first}
-          id={"first"}
+          id={'first'}
           value={first}
           onChange={handleInput}
         />
@@ -298,21 +259,21 @@ export const ContactForm = (props: FlexProps) => {
           isRequired
           isDisabled={isDisabled}
           isInvalid={isError.last}
-          id={"last"}
+          id={'last'}
           value={last}
           onChange={handleInput}
         />
       </Flex>
       <Flex
-        gap={[FIELD_SPACING, null, "1.5rem"]}
-        direction={["column", null, "row"]}
+        gap={[FIELD_SPACING, null, '1.5rem']}
+        direction={['column', null, 'row']}
         marginTop={FIELD_SPACING}
       >
         <ContactFormField
           isRequired
           isDisabled={isDisabled}
           isInvalid={isError.email}
-          id={"email"}
+          id={'email'}
           value={email}
           onChange={handleInput}
         />
@@ -321,7 +282,7 @@ export const ContactForm = (props: FlexProps) => {
           isRequired
           isDisabled={isDisabled}
           isInvalid={isError.phone}
-          id={"phone"}
+          id={'phone'}
           value={phone}
           onChange={handleInput}
         />
@@ -330,7 +291,7 @@ export const ContactForm = (props: FlexProps) => {
         isRequired
         isDisabled={isDisabled}
         isInvalid={isError.subject}
-        id={"subject"}
+        id={'subject'}
         value={subject}
         onChange={handleInput}
         marginTop={FIELD_SPACING}
@@ -340,7 +301,7 @@ export const ContactForm = (props: FlexProps) => {
         isRequired
         isDisabled={isDisabled}
         isInvalid={isError.message}
-        id={"message"}
+        id={'message'}
         value={message}
         onChange={handleInput}
         marginTop={FIELD_SPACING}
@@ -366,27 +327,36 @@ export const ContactForm = (props: FlexProps) => {
       <Button
         isDisabled={isDisabled}
         _hover={isDisabled ? {} : undefined}
-        variant={"contact"}
-        alignSelf={"flex-end"}
+        variant={'contact'}
+        alignSelf={'flex-end'}
         marginTop={FIELD_SPACING}
         onClick={handleSubmit}
-        _disabled={{ bgColor: "form.accent", padding: 0, cursor: "wait" }}
+        _disabled={{ bgColor: 'form.accent', padding: 0, cursor: 'wait' }}
       >
-        {isDisabled ? <PaperPlane /> : "Send Message"}
+        {isDisabled ? <PaperPlane /> : 'Send Message'}
       </Button>
-      <Text variant={"fineprint"}>
+      <Text variant={'fineprint'}>
         This site is protected by reCAPTCHA and the Google
-        <Link href="https://policies.google.com/privacy" color={"blue.500"}>
+        <Link
+          href='https://policies.google.com/privacy'
+          color={'blue.500'}
+        >
           {` Privacy Policy `}
-        </Link>{" "}
+        </Link>{' '}
         and
-        <Link href="https://policies.google.com/terms" color={"blue.500"}>
+        <Link
+          href='https://policies.google.com/terms'
+          color={'blue.500'}
+        >
           {` Terms of Service `}
-        </Link>{" "}
+        </Link>{' '}
         apply.
       </Text>
       {recapNeeded && (
-        <ReCAPTCHA recapRef={recaptchaValue} onChange={onChange} />
+        <ReCAPTCHA
+          recapRef={recaptchaValue}
+          onChange={onChange}
+        />
       )}
     </Flex>
   );
